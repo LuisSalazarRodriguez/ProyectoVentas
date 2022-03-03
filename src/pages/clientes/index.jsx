43e85e538975efacd858bff42dec3896efc020fd
Aldo Gerald Lázaro Rodriguez <88361238+Aldo-LR@users.noreply.github.com>
2022-03-03 13:45:06 -0500

    crud clientes - pedidos

--- src/pages/clientes/index.jsx
+++ src/pages/clientes/index.jsx
@@ -1,7 +1,281 @@
 import React from "react";
+import { useState } from 'react';
+
+import {FormGroup,ModalFooter,Button,Modal,ModalBody,ModalHeader} from "reactstrap";
 
 const PageClientes = () => {
-  return <div>CLIENTES</div>;
+
+  const dataClientes = [
+    {id:1,Nombre:"Juan",TipoDocumento:"DNI",Telefono:123123123,Dpto:"Arequipa",Prov:"Arequipa",Distrito:"Jose Luis Bustamante"},
+  ];
+  const [data, setData] = useState(dataClientes);   
+  const [modalInsertar,setModalInsertar] = useState(0);
+  const [modalEditar,setModalEditar] = useState(0);
+  const [modalEliminar,setModalEliminar] = useState(0);
+  const [clienteSeleccionado,setClienteSeleccionado] = useState({
+      id:'',
+      Nombre:'',
+      TipoDocumento:'',
+      Telefono:'',
+      Dpto:'',
+      Prov:'',
+      Distrito:'',
+  });
+const seleccionarCliente =(elemento,caso)=>{
+    setClienteSeleccionado(elemento);
+    (caso==='Editar')?setModalEditar(true):setModalEliminar(true);
+}
+const handleChange=e=>{
+    const{name,value}=e.target;
+    setClienteSeleccionado(prevState=>({
+        ...prevState,
+        [name]:value
+    }));
+}
+const editar=()=>{
+    var dataNueva=data;
+    dataNueva.map(cliente=>{
+        if(cliente.id===clienteSeleccionado.id){
+            
+          cliente.Nombre=clienteSeleccionado.Nombre;
+          cliente.TipoDocumento=clienteSeleccionado.TipoDocumento;
+          cliente.Telefono=clienteSeleccionado.Telefono;
+          cliente.Dpto=clienteSeleccionado.Dpto;
+          cliente.Prov=clienteSeleccionado.Prov;
+          cliente.Distrito=clienteSeleccionado.Distrito;
+        }
+    });
+    setData(dataNueva);
+    setModalEditar(false);
+}
+const eliminar=()=>{
+    setData(data.filter(cliente=>cliente.id!==clienteSeleccionado.id));
+    setModalEliminar(false);
+}
+const abrirModalInsertar=()=>{
+    setClienteSeleccionado(null);
+    setModalInsertar(true);
+}
+const insertar=()=>{
+    var valorInsertar=clienteSeleccionado;
+    valorInsertar.id=data[data.length-1].id+1;
+    var dataNueva=data;
+    dataNueva.push(valorInsertar);
+    setData(dataNueva);
+    setModalInsertar(false);
+}
+  return (
+  <div>
+    <h1> Listado de Clientes</h1>
+    <Button id="insertar" color="success" onClick={()=>abrirModalInsertar()}>Insertar Nuevo Lote</Button>
+    <div className="detalleClientes">
+      <table className="table table-striped">
+        <thead>
+          <tr>
+            <th>Nombre</th>
+            <th>Tipo Documento</th>
+            <th>Telefono</th>
+            <th>Departamento</th>
+            <th>Provincia</th>
+            <th>Distrito</th>
+            <th>Acciones</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.map(cliente=>(
+            <tr key={cliente.id}>
+              <td>{cliente.Nombre}</td>
+              <td>{cliente.TipoDocumento}</td>
+              <td>{cliente.Telefono}</td>
+              <td>{cliente.Dpto}</td>
+              <td>{cliente.Prov}</td>
+              <td>{cliente.Distrito}</td>
+              <td>
+                <Button color="primary" onClick={()=>seleccionarCliente(cliente,'Editar')}>Editar</Button>
+                <Button color="danger" onClick={()=>seleccionarCliente(cliente,'Eliminar')}>Eliminar</Button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+    <Modal isOpen={modalInsertar}>
+      <ModalHeader>
+        <div>
+          <h3>Insertar Nuevo Cliente</h3>
+        </div>
+      </ModalHeader>
+      <ModalBody>
+        <FormGroup>
+          <input
+          className="form-control" 
+          readonly 
+          type="hidden" 
+          value={clienteSeleccionado && clienteSeleccionado.id} 
+          onChange={handleChange}
+          value={data[data.length-1].id+1} />
+        </FormGroup>
+        <FormGroup>
+          <label>Nombre</label>
+          <input
+          className="form-control"
+          type="text"
+          name="Nombre"
+          value = {clienteSeleccionado ? clienteSeleccionado.Nombre : ''}
+          onChange={handleChange}
+          />
+        </FormGroup>
+        <FormGroup>
+          <label>Tipo Documento</label>
+          <input
+          className="form-control"
+          type="text"
+          name="TipoDocumento"
+          value = {clienteSeleccionado ? clienteSeleccionado.TipoDocumento : ''}
+          onChange={handleChange}
+          />
+        </FormGroup>
+        <FormGroup>
+          <label>Telefono</label>
+          <input
+          className="form-control"
+          type="text"
+          name="Telefono"
+          value = {clienteSeleccionado ? clienteSeleccionado.Telefono : ''}
+          onChange={handleChange}
+          />
+        </FormGroup>
+        <FormGroup>
+          <label>Departamento</label>
+          <input
+          className="form-control"
+          type="text"
+          name="Dpto"
+          value = {clienteSeleccionado ? clienteSeleccionado.Dpto : ''}
+          onChange={handleChange}
+          />
+        </FormGroup>
+        <FormGroup>
+          <label>Provincia</label>
+          <input
+          className="form-control"
+          type="text"
+          name="Prov"
+          value = {clienteSeleccionado ? clienteSeleccionado.Prov : ''}
+          onChange={handleChange}
+          />
+        </FormGroup>
+        <FormGroup>
+          <label>Distrito</label>
+          <input
+          className="form-control"
+          type="text"
+          name="Distrito"
+          value = {clienteSeleccionado ? clienteSeleccionado.Distrito : ''}
+          onChange={handleChange}
+          />
+        </FormGroup>
+      </ModalBody>
+      <ModalFooter>
+        <Button color="success" onClick={insertar}>Insertar</Button>
+        <Button color="danger" onClick={()=>setModalInsertar(false)}>Cancelar</Button>
+      </ModalFooter>
+    </Modal>
+
+    <Modal isOpen={modalEditar}>
+      <ModalHeader>
+        <div>
+          <h3>Editar Cliente</h3>
+        </div>
+      </ModalHeader>
+      <ModalBody>
+        <FormGroup>
+          <input
+          className="form-control"
+          readonly
+          type="hidden"
+          value={clienteSeleccionado && clienteSeleccionado.id}
+          onChange={handleChange}
+          />
+        </FormGroup>
+        <FormGroup>
+          <label>Nombre</label>
+          <input
+          className="form-control"
+          type="text"
+          name="Nombre"
+          value = {clienteSeleccionado ? clienteSeleccionado.Nombre : ''}
+          onChange={handleChange}
+          />
+        </FormGroup>
+        <FormGroup>
+          <label>Tipo Documento</label>
+          <input
+          className="form-control"
+          type="text"
+          name="TipoDocumento"
+          value = {clienteSeleccionado ? clienteSeleccionado.TipoDocumento : ''}
+          onChange={handleChange}
+          />
+        </FormGroup>
+        <FormGroup>
+          <label>Telefono</label>
+          <input
+          className="form-control"
+          type="text"
+          name="Telefono"
+          value = {clienteSeleccionado ? clienteSeleccionado.Telefono : ''}
+          onChange={handleChange}
+          />
+        </FormGroup>
+        <FormGroup>
+          <label>Departamento</label>
+          <input
+          className="form-control"
+          type="text"
+          name="Dpto"
+          value = {clienteSeleccionado ? clienteSeleccionado.Dpto : ''}
+          onChange={handleChange}
+          />
+        </FormGroup>
+        <FormGroup>
+          <label>Provincia</label>
+          <input
+          className="form-control"
+          type="text"
+          name="Prov"
+          value = {clienteSeleccionado ? clienteSeleccionado.Prov : ''}
+          onChange={handleChange}
+          />
+        </FormGroup>
+        <FormGroup>
+          <label>Distrito</label>
+          <input
+          className="form-control"
+          type="text"
+          name="Distrito"
+          value = {clienteSeleccionado ? clienteSeleccionado.Distrito : ''}
+          onChange={handleChange}
+          />
+        </FormGroup>
+      </ModalBody>
+      <ModalFooter>
+        <Button color="success" onClick={editar}>Editar</Button>
+        <Button color="danger" onClick={()=>setModalEditar(false)}>Cancelar</Button>
+      </ModalFooter>
+    </Modal>
+
+    <Modal isOpen={modalEliminar}>
+      <ModalBody>
+        ¿Estas seguro que deseas eliminar a {clienteSeleccionado && clienteSeleccionado.Nombre} ?
+      </ModalBody>
+      <ModalFooter>
+        <Button color="danger" onClick={eliminar}>Eliminar</Button>
+        <Button color="secondary" onClick={()=>setModalEliminar(false)}>Cancelar</Button>
+      </ModalFooter>
+    </Modal>
+  </div>
+  )
 };
 
 export { PageClientes };
--- src/pages/pedidos/index.jsx
+++ src/pages/pedidos/index.jsx
@@ -1,11 +1,305 @@
-import React from 'react';
+import React from "react";
+import { useState } from 'react';
+
+
+import {FormGroup,ModalFooter,Button,Modal,ModalBody,ModalHeader} from "reactstrap";
 
 const PagePedidos = () => {
-    return (
+
+  const dataPedido = [
+    {id:1,Producto:"Memoria USB",UM:"Unidad",Moneda:123123123,Precio:202022,Cantidad:1,SubTotal:12333,Total:3232323},
+  ];
+  const [data, setData] = useState(dataPedido);   
+  const [modalInsertar,setModalInsertar] = useState(0);
+  const [modalEditar,setModalEditar] = useState(0);
+  const [modalEliminar,setModalEliminar] = useState(0);
+  const [productoSeleccionado,setProductoSeleccionado] = useState({
+      id:'',
+      Producto:'',
+      UM:'',
+      Moneda:'',
+      Precio:'',
+      Cantidad:'',
+      SubTotal:'',
+      Total:'',
+  });
+const seleccionarPedido =(elemento,caso)=>{
+    setProductoSeleccionado(elemento);
+    (caso==='Editar')?setModalEditar(true):setModalEliminar(true);
+}
+const handleChange=e=>{
+    const{name,value}=e.target;
+    setProductoSeleccionado(prevState=>({
+        ...prevState,
+        [name]:value
+    }));
+}
+const editar=()=>{
+    var dataNueva=data;
+    dataNueva.map(pedido=>{
+        if(pedido.id===productoSeleccionado.id){
+            
+            pedido.Producto=productoSeleccionado.Producto;
+            pedido.UM=productoSeleccionado.UM;
+            pedido.Moneda=productoSeleccionado.Moneda;
+            pedido.Precio=productoSeleccionado.Precio;
+            pedido.Cantidad=productoSeleccionado.Cantidad;
+            pedido.SubTotal=productoSeleccionado.SubTotal;
+            pedido.Total=productoSeleccionado.Total;
+        }
+    });
+    setData(dataNueva);
+    setModalEditar(false);
+}
+const eliminar=()=>{
+    setData(data.filter(pedido=>pedido.id!==productoSeleccionado.id));
+    setModalEliminar(false);
+}
+const abrirModalInsertar=()=>{
+    setProductoSeleccionado(null);
+    setModalInsertar(true);
+}
+const insertar=()=>{
+    var valorInsertar=productoSeleccionado;
+    valorInsertar.id=data[data.length-1].id+1;
+    var dataNueva=data;
+    dataNueva.push(valorInsertar);
+    setData(dataNueva);
+    setModalInsertar(false);
+}
+  return (
+  <div>
+    <h1> Listado de Pedidos</h1>
+    <Button id="insertar" color="success" onClick={()=>abrirModalInsertar()}>Insertar Nuevo Lote</Button>
+    <div className="detalleProducto">
+      <table className="table table-striped">
+        <thead>
+          <tr>
+            <th>Producto</th>
+            <th>UM</th>
+            <th>Moneda</th>
+            <th>Precio</th>
+            <th>Cantidad</th>
+            <th>SubTotal</th>
+            <th>Total</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.map(producto=>(
+            <tr key={producto.id}>
+              <td>{producto.Producto}</td>
+              <td>{producto.UM}</td>
+              <td>{producto.Moneda}</td>
+              <td>{producto.Precio}</td>
+              <td>{producto.Cantidad}</td>
+              <td>{producto.Subtotal}</td>
+              <td>{producto.Total}</td>
+              <td>
+                <Button color="primary" onClick={()=>seleccionarPedido(producto,'Editar')}>Editar</Button>
+                <Button color="danger" onClick={()=>seleccionarPedido(producto,'Eliminar')}>Eliminar</Button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+    <Modal isOpen={modalInsertar}>
+      <ModalHeader>
         <div>
-            PEDIDOS
+          <h3>Insertar Nuevo Producto</h3>
         </div>
-    );
+      </ModalHeader>
+      <ModalBody>
+        <FormGroup>
+          <input
+          className="form-control" 
+          readonly 
+          type="hidden" 
+          value={productoSeleccionado && productoSeleccionado.id} 
+          onChange={handleChange}
+          value={data[data.length-1].id+1} />
+        </FormGroup>
+        <FormGroup>
+          <label>Producto</label>
+          <input
+          className="form-control"
+          type="text"
+          name="Producto"
+          value = {productoSeleccionado ? productoSeleccionado.Producto : ''}
+          onChange={handleChange}
+          />
+        </FormGroup>
+        <FormGroup>
+          <label>UM</label>
+          <input
+          className="form-control"
+          type="text"
+          name="UM"
+          value = {productoSeleccionado ? productoSeleccionado.UM : ''}
+          onChange={handleChange}
+          />
+        </FormGroup>
+        <FormGroup>
+          <label>Moneda</label>
+          <input
+          className="form-control"
+          type="text"
+          name="Moneda"
+          value = {productoSeleccionado ? productoSeleccionado.Moneda : ''}
+          onChange={handleChange}
+          />
+        </FormGroup>
+        <FormGroup>
+          <label>Precio</label>
+          <input
+          className="form-control"
+          type="text"
+          name="Precio"
+          value = {productoSeleccionado ? productoSeleccionado.Precio : ''}
+          onChange={handleChange}
+          />
+        </FormGroup>
+        <FormGroup>
+          <label>Cantidad</label>
+          <input
+          className="form-control"
+          type="text"
+          name="Cantidad"
+          value = {productoSeleccionado ? productoSeleccionado.Cantidad : ''}
+          onChange={handleChange}
+          />
+        </FormGroup>
+        <FormGroup>
+          <label>SubTotal</label>
+          <input
+          className="form-control"
+          type="text"
+          name="SubTotal"
+          value = {productoSeleccionado ? productoSeleccionado.SubTotal : ''}
+          onChange={handleChange}
+          />
+        </FormGroup>
+        <FormGroup>
+          <label>Total</label>
+          <input
+          className="form-control"
+          type="text"
+          name="Total"
+          value = {productoSeleccionado ? productoSeleccionado.Total : ''}
+          onChange={handleChange}
+          />
+        </FormGroup>
+      </ModalBody>
+      <ModalFooter>
+        <Button color="success" onClick={insertar}>Insertar</Button>
+        <Button color="danger" onClick={()=>setModalInsertar(false)}>Cancelar</Button>
+      </ModalFooter>
+    </Modal>
+
+    <Modal isOpen={modalEditar}>
+      <ModalHeader>
+        <div>
+          <h3>Editar Pedido</h3>
+        </div>
+      </ModalHeader>
+      <ModalBody>
+        <FormGroup>
+          <input
+          className="form-control"
+          readonly
+          type="hidden"
+          value={productoSeleccionado && productoSeleccionado.id}
+          onChange={handleChange}
+          />
+        </FormGroup>
+        <FormGroup>
+          <label>Producto</label>
+          <input
+          className="form-control"
+          type="text"
+          name="Producto"
+          value = {productoSeleccionado ? productoSeleccionado.Producto : ''}
+          onChange={handleChange}
+          />
+        </FormGroup>
+        <FormGroup>
+          <label>UM</label>
+          <input
+          className="form-control"
+          type="text"
+          name="UM"
+          value = {productoSeleccionado ? productoSeleccionado.UM : ''}
+          onChange={handleChange}
+          />
+        </FormGroup>
+        <FormGroup>
+          <label>Moneda</label>
+          <input
+          className="form-control"
+          type="text"
+          name="Moneda"
+          value = {productoSeleccionado ? productoSeleccionado.Moneda : ''}
+          onChange={handleChange}
+          />
+        </FormGroup>
+        <FormGroup>
+          <label>Precio</label>
+          <input
+          className="form-control"
+          type="text"
+          name="Precio"
+          value = {productoSeleccionado ? productoSeleccionado.Precio : ''}
+          onChange={handleChange}
+          />
+        </FormGroup>
+        <FormGroup>
+          <label>Cantidad</label>
+          <input
+          className="form-control"
+          type="text"
+          name="Cantidad"
+          value = {productoSeleccionado ? productoSeleccionado.Cantidad : ''}
+          onChange={handleChange}
+          />
+        </FormGroup>
+        <FormGroup>
+          <label>SubTotal</label>
+          <input
+          className="form-control"
+          type="text"
+          name="SubTotal"
+          value = {productoSeleccionado ? productoSeleccionado.SubTotal : ''}
+          onChange={handleChange}
+          />
+        </FormGroup>
+        <FormGroup>
+          <label>Total</label>
+          <input
+          className="form-control"
+          type="text"
+          name="Total"
+          value = {productoSeleccionado ? productoSeleccionado.Total : ''}
+          onChange={handleChange}
+          />
+        </FormGroup>
+      </ModalBody>
+      <ModalFooter>
+        <Button color="success" onClick={editar}>Editar</Button>
+        <Button color="danger" onClick={()=>setModalEditar(false)}>Cancelar</Button>
+      </ModalFooter>
+    </Modal>
+
+    <Modal isOpen={modalEliminar}>
+      <ModalBody>
+        ¿Estas seguro que deseas eliminar a {productoSeleccionado && productoSeleccionado.Producto} ?
+      </ModalBody>
+      <ModalFooter>
+        <Button color="danger" onClick={eliminar}>Eliminar</Button>
+        <Button color="secondary" onClick={()=>setModalEliminar(false)}>Cancelar</Button>
+      </ModalFooter>
+    </Modal>
+  </div>
+  )
 };
 
 export { PagePedidos };
